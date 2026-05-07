d69a98b00d8a7cb7dc62f28e80f9d349166a27fc
TaoYu <19323648564@163.com>
2026-05-07 17:50:23 +0800

    历史记录，断点

---
+++ 05.langgraph学习/11time_travel.py
@@ -0,0 +1,55 @@
+from langgraph.types import interrupt, Command
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import InMemorySaver
+
+
+class State(TypedDict):
+    value: list[str]
+
+
+def ask_human(state: State):
+    ###interrupt中断，执行到这边会停止
+    answer = interrupt("What is your name?")
+    return {"value": state['value'] + [f"name: {answer}!"]}
+
+
+def ask_age(state: State):
+    age = interrupt("How old are you?")
+    return {"value": state['value'] + [f"age:{age}"]}
+
+
+def final_step(state: State):
+    return {"value": state['value'] + ["Done"]}
+
+
+graph = (
+    StateGraph(State)
+    .add_node("ask_human", ask_human)
+    .add_node("ask_age", ask_age)
+    .add_node("final_step", final_step)
+    .add_edge(START, "ask_human")
+    .add_edge("ask_human", "ask_age")
+    .add_edge("ask_age", "final_step")
+    .add_edge("final_step", END)
+    .compile(checkpointer=InMemorySaver())
+)
+
+config = {"configurable": {"thread_id": "1"}}
+
+graph.invoke({"value": []}, config)  ###首次执行interrupt中断
+graph.invoke(Command(resume="Alice"), config)  ###首次执行输入的值{"value": [hello Alice，done]}
+res = graph.invoke(Command(resume="22"), config) ###多层断点
+print(res)
+
+
+history = list(graph.get_state_history(config))  ###获取所有的历史状态
+before_ask = [s for s in history if s.next == ("ask_human",)][-1]  ##找到 interrupt 前状态
+print([s for s in history if s.next == ("ask_human",)][-1])
+
+# replay_result = graph.invoke(None, before_ask.config)  ##time-travel，直接从checkponit之前执行也就是即将执行ask_human
+fork_config = graph.update_state(before_ask.config, {"value": ["forked"]})  ###状态分叉，创建一个新的focked分支
+fork_result = graph.invoke(None, fork_config)  ###interrupt再次中断
+graph.invoke(Command(resume="Bob"), fork_config)  ####{"value": ["forked"，"Bob","DONE"]}
+res1 = graph.invoke(Command(resume="12"), config)
+print(res1)
