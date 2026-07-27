f38e0dc076fd86b859a6150d7e37447d41697801
ty <19323648564@163.com>
2026-07-27 11:20:26 +0800

    feat:目录调整

--- 01.RAG学习/11通过ollamallm调用.py
+++
@@ -1,8 +0,0 @@
-#langchain_allama
-from langchain_ollama import  OllamaLLM
-
-model = OllamaLLM(
-    model="qwen3:4b"
-)
-
-model.invoke(input="你是谁")
--- 01langchain学习/03多模态.py
+++
@@ -1,5 +0,0 @@
-from ipywidgets import FileUpload
-from IPython.display import display
-
-upload = FileUpload(accept='image/*', multipart=False)
-display(upload)
