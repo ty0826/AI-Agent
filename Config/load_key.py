184bb9593b6da6e4f4ca7411667575c6e810da7e
ty <19323648564@163.com>
2026-07-31 15:09:37 +0800

    feat:llamaindex组件使用

---
+++ Config/load_key.py
@@ -0,0 +1,3 @@
+open_key='sk-ws-H.EIIEHIL.BWhG.MEYCIQCIRTABpot3xBg8nszsSNXivmFg81eX8b1MJOCJkoRvbwIhAPEQGa0Y3nXXnmvGBgVpKNkzssZRtz0LUaye6AT3ifY5'
+base_url='https://dashscope.aliyuncs.com/compatible-mode/v1'
+base_model = 'qwen3-max'
