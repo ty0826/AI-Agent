6ce0b373e136f3dc4c42f3f3f95a90d6ff386284
ty <19323648564@163.com>
2026-08-10 16:33:00 +0800

    fix:LLamaindex更新

--- Config/load_key.py
+++ Config/load_key.py
@@ -1,3 +1,3 @@
-open_key='sk-ws-H.EIIEHIL.BWhG.MEYCIQCIRTABpot3xBg8nszsSNXivmFg81eX8b1MJOCJkoRvbwIhAPEQGa0Y3nXXnmvGBgVpKNkzssZRtz0LUaye6AT3ifY5'
-base_url='https://dashscope.aliyuncs.com/compatible-mode/v1'
+open_key='sk-ws-H.ERIXDPE.oRnA.MEQCIGsIWNvhYPZ23h53u2X4y1ug6S5wsjI6lD0d7EzRYrysAiAEluG32ErPth_fgqaW4VSbyyxNMfl5NoAgwvxHhsb-aw'
+base_url='https://ws-we307bts4mdsmvys.cn-beijing.maas.aliyuncs.com/compatible-mode/v1'
 base_model = 'qwen3-max'
